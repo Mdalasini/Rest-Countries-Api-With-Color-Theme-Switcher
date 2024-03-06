255a1a38b53bff5498ddf8ccbe2f879bac76fda8
Jimmy772 <bobcook772@outlook.com>
2024-03-06 16:40:20 +0300

    chore: configured tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,30 @@ module.exports = {
       'xl': '1440px',
       // => @media (min-width: 1440px) { ... }
     },
-    extend: {},
+    extend: {
+      color : {
+        'dark-mode': {
+          'dark-blue': 'hsl(209, 23%, 22%)',
+          'very-dark-blue': 'hsl(207, 26%, 17%)',
+        },
+        'light-mode': {
+          'dark-blue': 'hsl(200, 15%, 8%)',
+          'dark-gray': 'hsl(0, 0%, 52%)',
+          'very-light-gray': 'hsl(0, 0%, 98%)'
+        },
+        'neutral': {
+          'white': 'hsl(0, 0%, 100%)'
+        }
+      }
+    },
+    fontFamily: {
+      'sans': ['"Nunito Sans", sans-serif']
+    },
+    fontWeight: {
+      'light': '300',
+      'normal': '600',
+      'bold': '800'
+    }
   },
   plugins: [],
 }
